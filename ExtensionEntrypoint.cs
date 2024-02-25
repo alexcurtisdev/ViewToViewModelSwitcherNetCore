ac0616ec37292faf9fd2a620cdde7af1866ddb41
Alex <alexcurtisphotography@gmail.com>
2024-02-25 10:13:17 +0000

    Add project files.

---
+++ ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace ViewToViewModelSwitcherNetCore
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "ViewToViewModelSwitcherNetCore.fdb8f460-25f1-4164-85b2-f81e0a5cbb86",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Publisher name",
+                    displayName: "ViewToViewModelSwitcherNetCore",
+                    description: "Extension description"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}

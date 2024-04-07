0a9f86ea26042b3292e3c7da1f2c9ee30c801df2
Alex <alexcurtisphotography@gmail.com>
2024-04-07 22:08:40 +0100

    Set version to 2.0.0.0 since it's now coded in .NetCore instead of version 1.5 which was .NetFramework

--- ExtensionEntrypoint.cs
+++ ExtensionEntrypoint.cs
@@ -11,7 +11,7 @@ namespace ViewToViewModelSwitcher
         {
             Metadata = new(
                     id: "ViewToViewModelSwitcher.fb2787f7-8b89-4cee-9e76-02ff6486f4f5",
-                    version: this.ExtensionAssemblyVersion,
+                    version: new Version(2,0,0,0),
                     publisherName: "Alex Curtis",
                     displayName: "MVVM View To View Model Switcher",
                     description: "A quick way to switch between a view and a view model"),

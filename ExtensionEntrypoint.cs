8487096d1e20c753f656665989aed8905f0c0bb6
Alex <alexcurtisphotography@gmail.com>
2024-04-08 20:18:02 +0100

    Removed unused using and updated the readme

--- ExtensionEntrypoint.cs
+++ ExtensionEntrypoint.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.Extensibility;
-using System.Runtime.ConstrainedExecution;
 
 namespace ViewToViewModelSwitcher
 {
@@ -11,7 +10,7 @@ namespace ViewToViewModelSwitcher
         {
             Metadata = new(
                     id: "ViewToViewModelSwitcher.fb2787f7-8b89-4cee-9e76-02ff6486f4f5",
-                    version: new Version(2,0,0,0),
+                    version: new Version(2,0,0,1),
                     publisherName: "Alex Curtis",
                     displayName: "MVVM View To View Model Switcher",
                     description: "A quick way to switch between a view and a view model"),
